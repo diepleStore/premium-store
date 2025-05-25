b4a1bc6ef34c4f75793497800d433181e235419a
Dieple <dieplehouse@gmail.com>
2025-05-25 11:27:17 +0700

    home ui base

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,86 @@
 import { ImageTrailHero } from "@/components/banner/dieplehouse-banner";
 import { SaleBannerHome } from "@/components/banner/sale-banner";
+import EmblaCarousel from "@/components/home-page/EmblaCarousel";
 import Incentives from "@/components/home-page/incentive";
 import SaleProduct from "@/components/home-page/sale-product";
 import TalkContent from "@/components/home-page/talk-content";
 import TrendingProduct from "@/components/home-page/trending-product";
+import { EmblaOptionsType } from 'embla-carousel'
 
 export default async function Home() {
+  const OPTIONS: EmblaOptionsType = {
+    dragFree: true,
+    containScroll: 'keepSnaps',
+    watchSlides: false,
+    watchResize: false
+  }
+  const SLIDE_COUNT = 5
+  const SLIDES = Array.from(Array(SLIDE_COUNT).keys())
+
+
   return (
     <>
-      <ImageTrailHero />
+      <div className="bg-[#D9D9D9] p-4 flex items-center justify-center text-center">
+        <span className="text-bold text-base md:text-3xl lg:text-4xl xl:text-5xl text-black">
+          IT’S NOT ABOUT BRAND <span className="font-sans">-</span> IT’S ABOUT STYLE
+        </span>
+      </div>
+
+      <div className="w-full relative bg-[#272525]">
+        <EmblaCarousel slides={SLIDES} options={OPTIONS} />
+      </div>
+
+      <div className="bg-[#FFFFFF] px-2 py-10 sm:py-22  flex flex-col items-center justify-center text-center">
+        <span className="text-bold text-lg md:text-3xl lg:text-4xl xl:text-5xl text-black font-bold">
+          " TỪ CÔ CHỦ NHỎ ĐẾN ĐẠI SỨ BÁN HÀNG "
+        </span>
+
+        <span className="max-w-[1000px] xl:max-w-[1200px] px-4 font-['Mont'] text-base md:text-xl lg:text-2xl xl:text-3xl text-black mt-4">
+          Năm 2014, Diệp chỉ là một newbie tập tành khởi nghiệp với những cửa hàng thời trang thanh lý nhỏ xíu. Làm nghề ký gửi, tức là giúp khách chọn lọc và định giá sản phẩm nên ngày qua ngày, Diệp học được cách quan sát, lắng nghe và hiểu rất rõ thói quen tiêu dùng của mọi người.
+        </span>
+      </div>
+
+      <div className="w-full relative bg-[#FFFCF7]">
+        <EmblaCarousel slides={SLIDES} options={OPTIONS} />
+      </div>
+
+      <div className="flex flex-col items-center justify-center text-center px-2 py-10 sm:py-22">
+        <span className="text-bold text-3xl md:text-5xl lg:text-6xl xl:text-7xl text-black font-bold mb-8 md:mb-18">
+          DIỆP LÊ CHOICES
+        </span>
+        <img
+          src={'/assets/icons/show-more-button.svg'}
+          alt='DiepLeHouse'
+          className='w-[120px] h-[35px] sm:h-[50px] sm:w-[140px]'
+        />
+      </div>
+
+      <div className="flex flex-col items-center justify-center text-center px-2 py-10 sm:py-22">
+        <span className="text-bold text-3xl md:text-5xl lg:text-6xl xl:text-7xl text-black font-bold mb-8 md:mb-18">
+          DIỆP LÊ COLLAB
+        </span>
+        <img
+          src={'/assets/icons/show-more-button.svg'}
+          alt='DiepLeHouse'
+          className='w-[120px] h-[35px] sm:h-[50px] sm:w-[140px]'
+        />
+      </div>
+
+
+      <div className="flex flex-col items-center justify-center text-center px-2 py-10 sm:py-22">
+        <span className="text-bold text-3xl md:text-5xl lg:text-6xl xl:text-7xl text-black font-bold mb-8 md:mb-18">
+          DIỆP LÊ PASS
+        </span>
+        <img
+          src={'/assets/icons/show-more-button.svg'}
+          alt='DiepLeHouse'
+          className='w-[120px] h-[35px] sm:h-[50px] sm:w-[140px]'
+        />
+      </div>
+      <SaleProduct />
+
+
+      {/* <ImageTrailHero />
       <SaleBannerHome />
       <main className="flex-1 mx-auto flex flex-col px-4">
         <TrendingProduct />
@@ -18,7 +90,7 @@ export default async function Home() {
           <Incentives />
         </div>
       </main>
-      <TalkContent />
+      <TalkContent /> */}
     </>
   );
 }
--- components/home-page/sale-product.tsx
+++ components/home-page/sale-product.tsx
@@ -1,117 +1,80 @@
 const products = [
-    {
-      id: 1,
-      name: 'Leather Long Wallet',
-      color: 'Natural',
-      price: '$75',
-      href: '#',
-      imageSrc: 'https://tailwindcss.com/plus-assets/img/ecommerce-images/home-page-04-trending-product-02.jpg',
-      imageAlt: 'Hand stitched, orange leather long wallet.',
-    },
-    {
-      id: 2,
-      name: 'Leather Long Wallet',
-      color: 'Natural',
-      price: '$75',
-      href: '#',
-      imageSrc: 'https://tailwindcss.com/plus-assets/img/ecommerce-images/home-page-04-trending-product-02.jpg',
-      imageAlt: 'Hand stitched, orange leather long wallet.',
-    },
-    {
-      id: 3,
-      name: 'Leather Long Wallet',
-      color: 'Natural',
-      price: '$75',
-      href: '#',
-      imageSrc: 'https://tailwindcss.com/plus-assets/img/ecommerce-images/home-page-04-trending-product-02.jpg',
-      imageAlt: 'Hand stitched, orange leather long wallet.',
-    },
-    {
-      id: 4,
-      name: 'Leather Long Wallet',
-      color: 'Natural',
-      price: '$75',
-      href: '#',
-      imageSrc: 'https://tailwindcss.com/plus-assets/img/ecommerce-images/home-page-04-trending-product-02.jpg',
-      imageAlt: 'Hand stitched, orange leather long wallet.',
-    },
-    {
-      id: 5,
-      name: 'Leather Long Wallet',
-      color: 'Natural',
-      price: '$75',
-      href: '#',
-      imageSrc: 'https://tailwindcss.com/plus-assets/img/ecommerce-images/home-page-04-trending-product-02.jpg',
-      imageAlt: 'Hand stitched, orange leather long wallet.',
-    },
-    {
-      id: 6,
-      name: 'Leather Long Wallet',
-      color: 'Natural',
-      price: '$75',
-      href: '#',
-      imageSrc: 'https://tailwindcss.com/plus-assets/img/ecommerce-images/home-page-04-trending-product-02.jpg',
-      imageAlt: 'Hand stitched, orange leather long wallet.',
-    },
-    {
-      id: 7,
-      name: 'Leather Long Wallet',
-      color: 'Natural',
-      price: '$75',
-      href: '#',
-      imageSrc: 'https://tailwindcss.com/plus-assets/img/ecommerce-images/home-page-04-trending-product-02.jpg',
-      imageAlt: 'Hand stitched, orange leather long wallet.',
-    },
-    {
-      id: 8,
-      name: 'Leather Long Wallet',
-      color: 'Natural',
-      price: '$75',
-      href: '#',
-      imageSrc: 'https://tailwindcss.com/plus-assets/img/ecommerce-images/home-page-04-trending-product-02.jpg',
-      imageAlt: 'Hand stitched, orange leather long wallet.',
-    },
-    // More products...
-  ]
-  
-  export default function SaleProduct() {
-    return (
-      <div className="bg-white">
-        <div className="mx-auto max-w-2xl py-8 sm:py-16 lg:max-w-7xl">
-          <div className="md:flex md:items-center md:justify-between">
-            <h2 className="text-2xl font-bold tracking-tight text-gray-900 uppercase">Sản phẩm Sale Mới</h2>
-            <a href="/products" className="hidden text-xl font-medium text-indigo-600 hover:text-indigo-500 md:block">
-              Tất cả sản phẩm
-              <span aria-hidden="true"> &rarr;</span>
-            </a>
-          </div>
-  
-          <div className="mt-6 grid grid-cols-2 gap-x-4 gap-y-10 sm:gap-x-6 md:grid-cols-4 md:gap-y-10 lg:gap-x-8">
-            {products.map((product) => (
-              <div key={product.id} className="group relative">
-                <div className="h-56 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:h-72 xl:h-80">
-                  <img alt={product.imageAlt} src={product.imageSrc} className="size-full object-cover" />
-                </div>
-                <h3 className="mt-4 text-base text-gray-700">
-                  <a href={product.href}>
-                    <span className="absolute inset-0" />
-                    {product.name}
-                  </a>
-                </h3>
-                <p className="mt-1 text-base text-gray-500">{product.color}</p>
-                <p className="mt-1 text-base font-medium text-gray-900">{product.price}</p>
-              </div>
-            ))}
-          </div>
-  
-          <div className="mt-8 text-xl md:hidden">
-            <a href="/products" className="font-medium text-indigo-600 hover:text-indigo-500">
-              Tất cả sản phẩm
-              <span aria-hidden="true"> &rarr;</span>
-            </a>
-          </div>
+  {
+    id: 1,
+    name: 'BỘ SỮA CHỐNG NẮNG DƯỠNG DA DỊU NHẸ - 60ML',
+    color: 'Natural',
+    price: '890.000đ',
+    href: '#',
+    imageSrc: 'https://tailwindcss.com/plus-assets/img/ecommerce-images/home-page-04-trending-product-02.jpg',
+    imageAlt: 'Hand stitched, orange BỘ SỮA CHỐNG NẮNG DƯỠNG DA DỊU NHẸ - 60ML.',
+  },
+  {
+    id: 2,
+    name: 'BỘ SỮA CHỐNG NẮNG DƯỠNG DA DỊU NHẸ - 60ML',
+    color: 'Natural',
+    price: '890.000đ',
+    href: '#',
+    imageSrc: 'https://tailwindcss.com/plus-assets/img/ecommerce-images/home-page-04-trending-product-02.jpg',
+    imageAlt: 'Hand stitched, orange BỘ SỮA CHỐNG NẮNG DƯỠNG DA DỊU NHẸ - 60ML.',
+  },
+  {
+    id: 3,
+    name: 'BỘ SỮA CHỐNG NẮNG DƯỠNG DA DỊU NHẸ - 60ML',
+    color: 'Natural',
+    price: '890.000đ',
+    href: '#',
+    imageSrc: 'https://tailwindcss.com/plus-assets/img/ecommerce-images/home-page-04-trending-product-02.jpg',
+    imageAlt: 'Hand stitched, orange BỘ SỮA CHỐNG NẮNG DƯỠNG DA DỊU NHẸ - 60ML.',
+  },
+  {
+    id: 4,
+    name: 'BỘ SỮA CHỐNG NẮNG DƯỠNG DA DỊU NHẸ - 60ML',
+    color: 'Natural',
+    price: '890.000đ',
+    href: '#',
+    imageSrc: 'https://tailwindcss.com/plus-assets/img/ecommerce-images/home-page-04-trending-product-02.jpg',
+    imageAlt: 'Hand stitched, orange BỘ SỮA CHỐNG NẮNG DƯỠNG DA DỊU NHẸ - 60ML.',
+  },
+  // More products...
+]
+
+export default function SaleProduct() {
+  return (
+    <div className="bg-[#FFFCF7]">
+      <div className="px-4 mx-auto max-w-2xl py-8 sm:py-16 lg:max-w-7xl">
+        <div className="flex items-center justify-between text-center">
+          <span className="text-bold w-full text-center text-lg md:text-3xl lg:text-4xl xl:text-5xl text-black font-bold">
+            SẢN PHẨM GIÁ TỐT
+          </span>
+          {/* <a href="/products" className="hidden text-xl font-medium text-indigo-600 hover:text-indigo-500 md:block">
+            Tất cả sản phẩm
+            <span aria-hidden="true"> &rarr;</span>
+          </a> */}
         </div>
+
+        <div className="mt-8 grid grid-cols-2 gap-x-4 gap-y-10 sm:gap-x-6 md:grid-cols-4 md:gap-y-10 lg:gap-x-8">
+          {products.map((product) => (
+            <div key={product.id} className="group relative text-center font-['Mont']">
+              <div className="h-56 w-full overflow-hidden bg-[#D9D9D9] group-hover:opacity-75 lg:h-72 xl:h-80">
+                {/* <img alt={product.imageAlt} src={product.imageSrc} className="size-full object-cover" /> */}
+              </div>
+              <p className="mt-4 text-base text-black font-[600] font-['Mont'] text-center line-clamp-2 h-[50px]">
+                {product.name} {product.name}
+              </p>
+              <p className="mt-1 text-base font-['Mont'] text-center text-gray-900">{product.price}</p>
+            </div>
+          ))}
+        </div>
+
+        <div className="w-full flex items-center justify-center mt-8">
+          <img
+            src={'/assets/icons/show-more-button.svg'}
+            alt='DiepLeHouse'
+            className='w-[120px] h-[35px] sm:h-[50px] sm:w-[140px]'
+          />
+        </div>
+
       </div>
-    )
-  }
-  
+    </div>
+  )
+}
